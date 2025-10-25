60b1927d25163b76be786f4477211e6d14fc4993
Едуард bc2024-5 <eduardcer2709@gmail.com>
2025-10-25 11:28:17 +0300

    Початковий коміт: базова структура проєкту з package.json та головним файлом

---
+++ main.js
@@ -0,0 +1,20 @@
+﻿#!/usr/bin/env node
+
+import { Command } from 'commander';
+import http from 'http';
+import fs from 'fs/promises';
+import path from 'path';
+import superagent from 'superagent';
+
+const program = new Command();
+
+program
+    .requiredOption('-h, --host <host>', 'адреса сервера')
+    .requiredOption('-p, --port <port>', 'порт сервера')
+    .requiredOption('-c, --cache <path>', 'шлях до директорії кешу');
+
+program.parse(process.argv);
+const options = program.opts();
+
+console.log('Проксі-сервер запускається...');
+console.log('Налаштування:', options);
